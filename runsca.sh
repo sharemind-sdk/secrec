6d75c9509e90a94b37b20fe6f0f4976e87653aa4
Jaak Ristioja <jaak.ristioja@cyber.ee>
2010-03-30 09:28:40 +0000

    Added simple script to run sca from the project directory without having to specify LD_LIBRARY_PATH.
    git-svn-id: svn+ssh://stacc/svn/secrecide/scc/trunk@50 7cd5a911-1356-4d01-8be5-57b866fed6b5

---
+++ runsca.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+D=$(cd `dirname $0` && pwd)
+E=${D}/bin/sca
+L=${D}/lib
+
+if [ ! -e "$E" ]; then
+    echo "$E does not appear to be built." >&2
+    Q=${D}/scc.pro
+    if [ ! -e "$Q" ]; then
+        echo "In addition no project file was found in $D" >&2
+        exit 1
+    fi
+    M=${D}/Makefile
+    echo -n "You should probably run " >&2
+    if [ ! -e "$M" ]; then
+        echo -n "\"qmake\" and " >&2
+	fi
+	echo "\"make\" in ${D}." >&2
+    exit 1
+fi
+LD_LIBRARY_PATH=${L} ${E}
