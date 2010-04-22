98ee26f22a6fdd10b48ef358868d42e89fc15795
Jaak Ristioja <jaak.ristioja@cyber.ee>
2010-04-22 11:59:15 +0000

    Sync
    git-svn-id: svn+ssh://stacc/svn/secrecide/scc/trunk@112 7cd5a911-1356-4d01-8be5-57b866fed6b5

--- include/libscc/reachingdefinitions.h
+++ include/libscc/reachingdefinitions.h
@@ -1,8 +1,7 @@
 #ifndef REACHINGDEFINITIONS_H
 #define REACHINGDEFINITIONS_H
 
-#include <set>
-#include <vector>
+#include <map>
 
 
 namespace SecreC {
@@ -11,13 +10,67 @@ class Symbol;
 class ICodeList;
 class Imop;
 
+class ReachingDefinition {
+    public: /* Types: */
+        enum Type { DIRECT, JUMP, CONDJUMP };
+
+    public: /* Methods: */
+        inline ReachingDefinition(Type type) : m_type(type) {}
+
+        inline Type type() const { return m_type; }
+
+    private: /* Fields: */
+        Type m_type;
+};
+
+class ReachingDefinitionDirect: public ReachingDefinition {
+    public: /* Methods: */
+        inline ReachingDefinitionDirect(const Imop *def)
+            : ReachingDefinition(ReachingDefinition::DIRECT), m_def(def) {}
+
+        inline const Imop *def() const { return m_def; }
+
+    private: /* Fields: */
+        const Imop *m_def;
+};
+
+class ReachingDefinitionJump: public ReachingDefinition {
+    public: /* Methods: */
+        inline ReachingDefinitionJump(ReachingDefinition *reaching)
+            : ReachingDefinition(ReachingDefinition::JUMP), m_reaching(reaching) {}
+
+        inline ReachingDefinition *reaching() const { return m_reaching; }
+
+    private: /* Fields: */
+        ReachingDefinition *m_reaching;
+};
+
+
+class ReachingDefinitionCondJump: public ReachingDefinition {
+    public: /* Methods: */
+        inline ReachingDefinitionCondJump(ReachingDefinition *reaching)
+            : ReachingDefinition(ReachingDefinition::CONDJUMP), m_reaching(reaching) {}
+
+        inline ReachingDefinition *reaching() const { return m_reaching; }
+
+    private: /* Fields: */
+        ReachingDefinition *m_reaching;
+};
+
 class ReachingDefinitions {
+    public: /* Types: */
+        typedef std::map<const Symbol*, const Imop*> RMapping;
+        typedef std::map<const Imop*, RMapping> Mapping;
+
     public: /* Methods: */
         ReachingDefinitions(const ICodeList &code);
         void run();
 
+    protected: /* Methods: */
+        bool step(const Imop *i);
+
     private: /* Fields: */
-        std::vector<std::set<Symbol*,Imop*> > m_defs;
+        Mapping m_ins;
         const ICodeList &m_code;
 };
 
--- src/libscc/reachingdefinitions.cpp
+++ src/libscc/reachingdefinitions.cpp
@@ -1,18 +1,44 @@
 #include "reachingdefinitions.h"
 
+#include "blocks.h"
 #include "icodelist.h"
+#include "imop.h"
 
 
 namespace SecreC {
+namespace {
+
+inline bool isLeader(const Imop *i) {
+    return *(i->block()->start) == i;
+}
+
+inline bool isEnd(const Imop *i) {
+    return *(i->block()->end) == i;
+}
+
+inline void makeOuts(ReachingDefinitions::RMapping &ins, const Imop *i) {
+    if ((i->type() & Imop::EXPR_MASK) == 0x0) return;
+    ins[i->dest()] = i;
+}
+
+} // anonymous namespace
+
+
 
 ReachingDefinitions::ReachingDefinitions(const ICodeList &code)
-    : m_defs(code.size()), m_code(code)
+    : m_code(code)
 {
     // Intentionally empty
 }
 
 void ReachingDefinitions::run() {
+    std::vector<bool> changed(m_code.size(), false);
+
     /// \todo
+}
+
+bool step(const Imop *i) {
+
 }
 
 } // namespace SecreC
