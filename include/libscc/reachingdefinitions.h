ea3224fdb077b3344e80326746407d79ed364d77
Jaak Ristioja <jaak.ristioja@cyber.ee>
2010-04-22 12:26:29 +0000

    Sync
    git-svn-id: svn+ssh://stacc/svn/secrecide/scc/trunk@113 7cd5a911-1356-4d01-8be5-57b866fed6b5

--- include/libscc/reachingdefinitions.h
+++ include/libscc/reachingdefinitions.h
@@ -1,7 +1,9 @@
 #ifndef REACHINGDEFINITIONS_H
 #define REACHINGDEFINITIONS_H
 
+#include <cassert>
 #include <map>
+#include <set>
 
 
 namespace SecreC {
@@ -12,12 +14,17 @@ class Imop;
 
 class ReachingDefinition {
     public: /* Types: */
-        enum Type { DIRECT, JUMP, CONDJUMP };
+        enum Type { DIRECT, CONDJUMP };
 
     public: /* Methods: */
         inline ReachingDefinition(Type type) : m_type(type) {}
 
         inline Type type() const { return m_type; }
+        virtual inline bool operator==(const ReachingDefinition &o)
+                const
+        {
+            return m_type == o.m_type;
+        }
 
     private: /* Fields: */
         Type m_type;
@@ -29,36 +36,43 @@ class ReachingDefinitionDirect: public ReachingDefinition {
             : ReachingDefinition(ReachingDefinition::DIRECT), m_def(def) {}
 
         inline const Imop *def() const { return m_def; }
+        inline bool operator==(const ReachingDefinition &o) const {
+            typedef ReachingDefinitionDirect RDD;
+            if (!ReachingDefinition::operator==(o)) return false;
+            assert(dynamic_cast<const RDD*>(&o) != 0);
+            return static_cast<const RDD&>(o).m_def == m_def;
+        }
 
     private: /* Fields: */
         const Imop *m_def;
 };
 
-class ReachingDefinitionJump: public ReachingDefinition {
-    public: /* Methods: */
-        inline ReachingDefinitionJump(ReachingDefinition *reaching)
-            : ReachingDefinition(ReachingDefinition::JUMP), m_reaching(reaching) {}
-
-        inline ReachingDefinition *reaching() const { return m_reaching; }
-
-    private: /* Fields: */
-        ReachingDefinition *m_reaching;
-};
-
-
 class ReachingDefinitionCondJump: public ReachingDefinition {
     public: /* Methods: */
-        inline ReachingDefinitionCondJump(ReachingDefinition *reaching)
-            : ReachingDefinition(ReachingDefinition::CONDJUMP), m_reaching(reaching) {}
+        inline ReachingDefinitionCondJump(const ReachingDefinition &reaching,
+                                          const Imop *condJump)
+            : ReachingDefinition(ReachingDefinition::CONDJUMP),
+              m_condJump(condJump), m_reaching(reaching) {}
 
-        inline ReachingDefinition *reaching() const { return m_reaching; }
+
+        inline const ReachingDefinition &reaching() const {
+            return m_reaching;
+        }
+        inline bool operator==(const ReachingDefinition &o) const {
+            typedef ReachingDefinitionCondJump RDCJ;
+            if (!ReachingDefinition::operator==(o)) return false;
+            assert(dynamic_cast<const RDCJ*>(&o) != 0);
+            return static_cast<const RDCJ&>(o).m_reaching == m_reaching;
+        }
 
     private: /* Fields: */
-        ReachingDefinition *m_reaching;
+        const Imop              *m_condJump;
+        const ReachingDefinition m_reaching;
 };
 
 class ReachingDefinitions {
     public: /* Types: */
+        typedef std::set<ReachingDefinition*> Defs;
         typedef std::map<const Symbol*, const Imop*> RMapping;
         typedef std::map<const Imop*, RMapping> Mapping;
 
