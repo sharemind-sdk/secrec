66043bad4c2fe514359e5e85533463a91bb83048
Jaak Ristioja <jaak.ristioja@cyber.ee>
2010-04-23 15:53:35 +0000

    Preliminary reaching definitions analysis.
    git-svn-id: svn+ssh://stacc/svn/secrecide/scc/trunk@116 7cd5a911-1356-4d01-8be5-57b866fed6b5

--- include/libscc/reachingdefinitions.h
+++ include/libscc/reachingdefinitions.h
@@ -1,91 +1,33 @@
 #ifndef REACHINGDEFINITIONS_H
 #define REACHINGDEFINITIONS_H
 
-#include <cassert>
 #include <map>
 #include <set>
 
 
 namespace SecreC {
 
+class Block;
 class Symbol;
-class ICodeList;
+class ICode;
 class Imop;
-
-class ReachingDefinition {
-    public: /* Types: */
-        enum Type { DIRECT, CONDJUMP };
-
-    public: /* Methods: */
-        inline ReachingDefinition(Type type) : m_type(type) {}
-
-        inline Type type() const { return m_type; }
-        virtual inline bool operator==(const ReachingDefinition &o)
-                const
-        {
-            return m_type == o.m_type;
-        }
-
-    private: /* Fields: */
-        Type m_type;
-};
-
-class ReachingDefinitionDirect: public ReachingDefinition {
-    public: /* Methods: */
-        inline ReachingDefinitionDirect(const Imop *def)
-            : ReachingDefinition(ReachingDefinition::DIRECT), m_def(def) {}
-
-        inline const Imop *def() const { return m_def; }
-        inline bool operator==(const ReachingDefinition &o) const {
-            typedef ReachingDefinitionDirect RDD;
-            if (!ReachingDefinition::operator==(o)) return false;
-            assert(dynamic_cast<const RDD*>(&o) != 0);
-            return static_cast<const RDD&>(o).m_def == m_def;
-        }
-
-    private: /* Fields: */
-        const Imop *m_def;
-};
-
-class ReachingDefinitionCondJump: public ReachingDefinition {
-    public: /* Methods: */
-        inline ReachingDefinitionCondJump(const ReachingDefinition &reaching,
-                                          const Imop *condJump)
-            : ReachingDefinition(ReachingDefinition::CONDJUMP),
-              m_condJump(condJump), m_reaching(reaching) {}
-
-
-        inline const ReachingDefinition &reaching() const {
-            return m_reaching;
-        }
-        inline bool operator==(const ReachingDefinition &o) const {
-            typedef ReachingDefinitionCondJump RDCJ;
-            if (!ReachingDefinition::operator==(o)) return false;
-            assert(dynamic_cast<const RDCJ*>(&o) != 0);
-            return static_cast<const RDCJ&>(o).m_reaching == m_reaching;
-        }
-
-    private: /* Fields: */
-        const Imop              *m_condJump;
-        const ReachingDefinition m_reaching;
-};
 
 class ReachingDefinitions {
     public: /* Types: */
-        typedef std::set<ReachingDefinition*> Defs;
-        typedef std::map<const Symbol*, const Imop*> RMapping;
-        typedef std::map<const Imop*, RMapping> Mapping;
+        typedef std::set<const Imop*>        Defs;
+        typedef std::map<const Symbol*,Defs> SDefs;
+        typedef std::map<const Block*,SDefs> BDM;
 
     public: /* Methods: */
-        ReachingDefinitions(const ICodeList &code);
+        ReachingDefinitions(const ICode &code);
         void run();
 
-    protected: /* Methods: */
-        bool step(const Imop *i);
+    private: /* Methods: */
+        bool makeOuts(const Block &i, const SDefs &in, SDefs &out);
 
     private: /* Fields: */
-        Mapping m_ins;
-        const ICodeList &m_code;
+        const ICode &m_code;
+        BDM          m_ins;
 };
 
 } // namespace SecreC
--- src/libscc/reachingdefinitions.cpp
+++ src/libscc/reachingdefinitions.cpp
@@ -1,44 +1,87 @@
 #include "reachingdefinitions.h"
 
-#include "blocks.h"
-#include "icodelist.h"
-#include "imop.h"
+#include "intermediate.h"
 
 
 namespace SecreC {
 namespace {
 
-inline bool isLeader(const Imop *i) {
-    return *(i->block()->start) == i;
+typedef ICodeList::const_iterator CLCI;
+
+inline bool isFirstImop(const Imop *i) {
+    return i->block()->firstImop() == i;
 }
 
-inline bool isEnd(const Imop *i) {
-    return *(i->block()->end) == i;
-}
-
-inline void makeOuts(ReachingDefinitions::RMapping &ins, const Imop *i) {
-    if ((i->type() & Imop::EXPR_MASK) == 0x0) return;
-    ins[i->dest()] = i;
+inline ReachingDefinitions::Defs &operator+=(ReachingDefinitions::Defs &dest,
+                                             const ReachingDefinitions::Defs &src)
+{
+    dest.insert(src.begin(), src.end());
+    return dest;
 }
 
 } // anonymous namespace
 
 
-
-ReachingDefinitions::ReachingDefinitions(const ICodeList &code)
+ReachingDefinitions::ReachingDefinitions(const ICode &code)
     : m_code(code)
 {
-    // Intentionally empty
+    assert(code.status() == ICode::OK);
+    assert(code.code().size() > 1);
+    assert(isFirstImop(code.code()[0]));
+    assert(code.code()[0]->block()->predecessors.empty());
+    assert(code.code()[0]->block()->predecessorsCondFalse.empty());
+    assert(code.code()[0]->block()->predecessorsCondTrue.empty());
+    assert(code.code()[0]->block()->predecessorsCall.empty());
+    assert(code.code()[0]->block()->predecessorsRet.empty());
 }
 
 void ReachingDefinitions::run() {
-    std::vector<bool> changed(m_code.size(), false);
+    typedef std::set<Block*>::const_iterator BSCI;
 
-    /// \todo
+    bool changed;
+
+    const std::vector<Block*> &bs = m_code.blocks().blocks();
+
+    BDM outs;
+
+    makeOuts(*bs[0], m_ins[bs[0]], outs[bs[0]]);
+    do {
+        changed = false;
+        for (size_t i = 1; i < bs.size(); i++) {
+            const Block *b = bs[i];
+            if (!b->reachable) continue;
+
+            // Recalculate input set:
+            m_ins[b].clear();
+            for (BSCI it = b->predecessors.begin(); it != b->predecessors.end(); it++) {
+                for (SDefs::const_iterator jt = outs[*it].begin(); jt != outs[*it].end(); jt++) {
+                    m_ins[b][(*jt).first] += (*jt).second;
+                }
+            }
+
+            // Recalculate output set:
+            if (makeOuts(*b, m_ins[b], outs[b])) {
+                changed = true;
+            }
+        }
+    } while (changed);
 }
 
-bool step(const Imop *i) {
+bool ReachingDefinitions::makeOuts(const Block &b, const SDefs &in, SDefs &out) {
+    SDefs old(out);
+    out = in;
 
+    for (Blocks::CCI it = b.start; it != b.end; it++) {
+        if ((*it)->type() == Imop::VARINTRO || (*it)->type() == Imop::PARAMINTRO
+            || ((*it)->type() & Imop::EXPR_MASK) != 0)
+        {
+            // Set this def:
+            Defs &d = out[(*it)->dest()];
+            d.clear();
+            d.insert(*it);
+        }
+    }
+    return old != out;
 }
 
 } // namespace SecreC
