6d213e58504f1f4d632b6663c6110452531c415f
Jaak Ristioja <jaak.ristioja@cyber.ee>
2010-05-25 14:13:46 +0000

    Fix reaching definitions and reaching jumps analysis for call pass edges.
    git-svn-id: svn+ssh://stacc/svn/secrecide/scc/trunk@152 7cd5a911-1356-4d01-8be5-57b866fed6b5

--- include/libscc/dataflowanalysis.h
+++ include/libscc/dataflowanalysis.h
@@ -90,6 +90,7 @@ class ReachingDefinitions: public ForwardDataFlowAnalysis {
         virtual void inFrom(const Block &from, const Block &to);
         virtual inline void inFromTrue(const Block &from, const Block &to) { inFrom(from, to); }
         virtual inline void inFromFalse(const Block &from, const Block &to) { inFrom(from, to); }
+        virtual inline void inFromCallPass(const Block &from, const Block &to) { inFrom(from, to); }
         virtual inline bool finishBlock(const Block &b) { return makeOuts(b, m_ins[&b], m_outs[&b]); }
         virtual inline void finish() { m_outs.clear(); }
 
@@ -117,6 +118,7 @@ public: /* Types: */
         virtual void inFrom(const Block &from, const Block &to);
         virtual void inFromTrue(const Block &from, const Block &to);
         virtual void inFromFalse(const Block &from, const Block &to);
+        virtual inline void inFromCallPass(const Block &from, const Block &to) { inFrom(from, to); }
         virtual bool finishBlock(const Block &b);
         virtual inline void finish() { m_outPos.clear(); m_outNeg.clear(); }
 
