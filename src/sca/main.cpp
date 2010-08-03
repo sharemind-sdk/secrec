057752af471d01a90d04ced04a66a30399356235
Jaak Randmets <jaak.randmets@stacc.ee>
2010-08-03 14:46:46 +0000

    Better command line controls.
    git-svn-id: svn+ssh://stacc/svn/secrecide/scc/trunk@189 7cd5a911-1356-4d01-8be5-57b866fed6b5

--- src/sca/main.cpp
+++ src/sca/main.cpp
@@ -1,5 +1,8 @@
 #include <cassert>
 #include <iostream>
+#include <cstring>
+#include <getopt.h>
+
 #include <libscc/blocks.h>
 #include <libscc/intermediate.h>
 #include <libscc/parser.h>
@@ -8,20 +11,94 @@
 
 using namespace std;
 
+void help (void) {
+  cout << 
+  "Usage: sca [options] [file]\n"\
+  "Options:\n"\
+  "  -h, --help           this help\n"\
+  "  -v, --verbose        some extra information\n"\
+  "      --print_ast      print abstract syntax tree\n"\
+  "  -a, --analysis       select analysis that you wish to enable\n"\
+  "                       Possible comma seperated values are:\n"\
+  "                       \"rd\" for reaching definitions\n"\
+  "                       \"rj\" for reaching jumps\n"\
+  "                       \"rdc\" for reaching declassify\n"\
+  << endl;
+}
+
 int main(int argc, char *argv[]) {
-    char *filename = 0;
-    if (argc > 1) {
-        filename = argv[1];
-    }
+    char* filename = 0;
+    int verbose_flag = 0;
+    int help_flag = 0;
+    int run_analysis = 1;
+    int print_ast = 0;
+    int rd_flag = 1, rj_flag = 1, rdc_flag = 1;
     SecreC::TreeNodeProgram *parseTree = 0;
 
     int parseResult;
+
+    while (1) {
+      static struct option options[] = {
+        {"verbose",      no_argument,       0,                  'v'},
+        {"help",         no_argument,       0,                  'h'},
+        {"print_ast",    no_argument,       &print_ast,          1 },
+        {"analysis",     optional_argument, 0,                  'a'},
+        {0, 0, 0, 0}
+      };
+
+      int option_index = 0;
+      int c = getopt_long (argc, argv, "vha::", options, &option_index);
+      char *str = optarg;
+
+      if (c == -1) {
+        break;
+      }
+
+      switch (c) {
+        case 0:  /* intentionally empty */ break;
+        case 'v':
+          verbose_flag = 1;
+          break;
+        case 'h':
+          help_flag = 1;
+          break;
+        case 'a':
+          rd_flag = rj_flag = rdc_flag = 0;
+          run_analysis = 0;
+          while (1) {
+            char const *token = strtok (str, ",");
+            if (token == 0) break;
+            if (strcmp (token, "rd") == 0)  { run_analysis = rd_flag = 1; }
+            if (strcmp (token, "rj") == 0)  { run_analysis = rj_flag = 1; }
+            if (strcmp (token, "rdc") == 0) { run_analysis = rdc_flag = 1; }
+            str = NULL;
+          }
+
+          break;
+        default:
+          help ();
+          return 1;
+      }
+    }
+
+    if (help_flag) {
+      help ();
+      return 0;
+    }
+
+    if (optind < argc) {
+      filename = argv[optind ++];
+    }
+
     if (filename == 0) {
         parseResult = sccparse(&parseTree);
     } else {
         FILE *f = fopen(filename, "r");
         if (f != NULL) {
-            cerr << "Parsing file: " << filename << endl;
+            if (verbose_flag) {
+              cerr << "Parsing file: " << filename << endl;
+            }
+
             parseResult = sccparse_file(f, &parseTree);
             fclose(f);
         } else {
@@ -29,38 +106,47 @@ int main(int argc, char *argv[]) {
             return 1;
         }
     }
+    
     fflush(stdout);
     fflush(stderr);
 
     if (parseResult == 0) {
         assert(parseTree != 0);
-        // Print AST:
-        // cout << parseTree->toString() << endl << endl;
+        if (print_ast) {
+          cout << parseTree->toString() << endl << endl;
+        }
 
         SecreC::ICode icode;
         icode.init(parseTree);
 
         if (icode.status() == SecreC::ICode::OK) {
-            cerr << "Valid intermediate code generated." << endl
-                 << icode.compileLog();
+            if (verbose_flag) {
+              cerr << "Valid intermediate code generated." << endl
+                   << icode.compileLog();
+            }
 
             // Print basic blocks:
             SecreC::Blocks bs;
             bs.init(icode.code());
-            cout << bs.toString() << std::endl;
+
+            if (verbose_flag) {
+              cout << bs.toString() << endl;
+            }
 
             // Run data flow analysis and print the results:
-            SecreC::DataFlowAnalysisRunner runner;
-            SecreC::ReachingDefinitions rd;
-            SecreC::ReachingJumps rj;
-            SecreC::ReachingDeclassify rdc;
-            runner.addAnalysis(&rd);
-            runner.addAnalysis(&rj);
-            runner.addAnalysis(&rdc);
-            runner.run(bs);
-            cout << rd.toString(bs) << std::endl;
-            cout << rj.toString(bs) << std::endl;
-            cout << rdc.toString() << std::endl;
+            if (run_analysis) {
+              SecreC::DataFlowAnalysisRunner runner;
+              SecreC::ReachingDefinitions rd;
+              SecreC::ReachingJumps rj;
+              SecreC::ReachingDeclassify rdc;
+              if (rd_flag)  runner.addAnalysis(&rd);
+              if (rj_flag)  runner.addAnalysis(&rj);
+              if (rdc_flag) runner.addAnalysis(&rdc);
+              runner.run(bs);
+              if (rd_flag)  cout << rd.toString(bs) << endl;
+              if (rj_flag)  cout << rj.toString(bs) << endl;
+              if (rdc_flag) cout << rdc.toString()  << endl;
+            }
         } else {
             cerr << "Error generating valid intermediate code." << endl
                  << icode.compileLog();
