442376021755efdf2ca814a8679d9bd740c19d5a
Jaak Randmets <jaak.randmets@stacc.ee>
2010-08-30 13:59:07 +0000

    Trivial change to assert.
    git-svn-id: svn+ssh://stacc/svn/secrecide/scc/trunk@220 7cd5a911-1356-4d01-8be5-57b866fed6b5

--- src/libscc/treenode.cpp
+++ src/libscc/treenode.cpp
@@ -2645,7 +2645,7 @@ ICode::Status TreeNodeStmtAssert::generateCode(ICodeList &code, SymbolTable &st,
     setFirstImop(e->firstImop());
 
     std::ostringstream os;
-    os << "assert failed: " << toString();
+    os << "assert failed at " << location();
     Imop *i = new Imop(this, Imop::ERROR, 0, (Symbol*) new std::string(os.str()));
     code.push_imop(i);
 
