f4fde5293f4512f6bbc3ec956bcffdaf599978bc
Jaak Randmets <jaak.randmets@cyber.ee>
2011-10-20 17:30:20 +0300

    Add file headers for scc.

--- src/scc/VMCode.h
+++ src/scc/VMCode.h
@@ -1,3 +1,12 @@
+/*
+ * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
+ *
+ * All rights are reserved. Reproduction in whole or part is prohibited
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
+ */
+
 #ifndef VM_CODE_H
 #define VM_CODE_H
 
--- src/scc/VMValue.h
+++ src/scc/VMValue.h
@@ -1,3 +1,12 @@
+/*
+ * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
+ *
+ * All rights are reserved. Reproduction in whole or part is prohibited
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
+ */
+
 #ifndef VM_VALUE_H
 #define VM_VALUE_H
 
