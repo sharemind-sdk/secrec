97ff6ae6db0af5ca9ff4bfa8bdbb0917490621b3
Jaak Randmets <jaak.randmets@cyber.ee>
2011-11-21 13:08:01 +0200

    And the header.

---
+++ include/libscc/context.h
@@ -0,0 +1,43 @@
+/*
+ * This file is a part of the Sharemind framework.
+ * Copyright (C) Cybernetica AS
+ *
+ * All rights are reserved. Reproduction in whole or part is prohibited
+ * without the written consent of the copyright owner. The usage of this
+ * code is subject to the appropriate license agreement.
+ */
+
+#ifndef CONTEXT_H
+#define CONTEXT_H
+
+#include "parser.h"
+
+#include <vector>
+
+namespace SecreC {
+
+class ContextImpl;
+
+class Context {
+private:
+
+    Context (const Context&); // DO NOT IMPLEMENT
+    void operator = (const Context&); // DO NOT IMPLEMENT
+
+public: /* Fields: */
+
+    Context ();
+    ~Context ();
+
+    inline ContextImpl* pImpl () {
+        return m_pImpl;
+    }
+
+private: /* Fields: */
+
+    ContextImpl* m_pImpl;
+};
+
+}
+
+#endif // CONTEXT_H
