1431adb449a3764f97f9b6f7b0878b1ed7d3cb25
Jaak Randmets <jaak.randmets@cyber.ee>
2012-03-13 18:49:28 +0200

    [scc] Directly allow bytecode generation.
    * Simply link against the libas.
    * Default mode is to generate the bytecode.
    * Assembler can be generated using the -S flag.

--- src/scc/main.cpp
+++ src/scc/main.cpp
@@ -14,101 +14,321 @@
 #include <iomanip>
 #include <fstream>
 
+#include <boost/optional.hpp>
 #include <boost/foreach.hpp>
 #include <boost/program_options.hpp>
 #include <boost/filesystem.hpp>
 #include <boost/iostreams/stream.hpp>
+#include <boost/iostreams/device/mapped_file.hpp>
+#include <boost/iostreams/device/file.hpp>
+#include <boost/iostreams/stream_buffer.hpp>
 
 #include <libscc/context.h>
 #include <libscc/intermediate.h>
 #include <libscc/treenode.h>
 #include <libscc/blocks.h>
 
+extern "C" {
+#include <sharemind/libas/assemble.h>
+#include <sharemind/libas/linker.h>
+#include <sharemind/libas/tokenizer.h>
+}
+
 #include "Compiler.h"
 
 using namespace std;
 using namespace SecreCC;
+
 namespace po = boost::program_options;
+namespace fs = boost::filesystem;
+namespace io = boost::iostreams;
 
-int main(int argc, char *argv[]) {
+namespace /* anonymous */ {
 
-    po::options_description desc ("Available options");
-    desc.add_options ()
-            ("help,h", "Display this help message.")
-            ("verbose,v", "Enable verbose output.")
-            ("include,I", po::value<vector<string > >(), "Directory for module search path.")
-            ("assemble,S", "Output assembly.")
-            ("compile,c", "Compile to bytecode.")
-            ("output,o", po::value<string>(), "Output file.")
-            ("input", po::value<string>(), "Input file.")
-            ;
-    po::positional_options_description p;
-    p.add("input", -1);
-    po::variables_map vm;
+/*
+ * RAII to remove a temporary path.
+ */
+class ScopedRemovePath : boost::noncopyable {
+public: /* Methods: */
+    ScopedRemovePath (const fs::path& path)
+        : m_path (path) { }
 
-    try {
-        po::store(po::command_line_parser (argc, argv).
-                  options (desc).positional (p).run (), vm);
-        po::notify(vm);
-    }
-    catch (const std::exception& e) {
-        std::cerr << e.what () << std::endl;
-        std::cerr << desc << std::endl;
-        return EXIT_FAILURE;
+    ~ScopedRemovePath () {
+        boost::system::error_code ec;
+        fs::remove (m_path, ec);
     }
 
-    bool verbose = false;
-    if (vm.count ("verbose")) {
-        verbose = true;
+private: /* Fields: */
+    const fs::path& m_path;
+};
+
+/*
+ * RAII for SharemindAssemblerTokens.
+ */
+class ScopedAsmTokens : boost::noncopyable {
+public:
+    explicit ScopedAsmTokens (SharemindAssemblerTokens* ts)
+        : m_ts (ts) { }
+
+    ~ScopedAsmTokens () {
+        SharemindAssemblerTokens_free (m_ts);
     }
 
-    if (vm.count ("help")) {
-        cout << desc << "\n";
-        return EXIT_SUCCESS;
+    SharemindAssemblerTokens* get () const { return m_ts; }
+private:
+    SharemindAssemblerTokens* const m_ts;
+};
+
+/*
+ * RAII for SharemindAssemblerLinkingUnits.
+ */
+class ScopedAsmLinkingUnits : public SharemindAssemblerLinkingUnits, boost::noncopyable {
+    ScopedAsmLinkingUnits (const ScopedAsmLinkingUnits&);
+    void operator = (const ScopedAsmLinkingUnits&);
+
+public:
+    ScopedAsmLinkingUnits () {
+        SharemindAssemblerLinkingUnits_init (this);
     }
 
-    /* Get output stream: */
-    ostream os (cout.rdbuf ());
-    ofstream fout;
-    if (vm.count ("output")) {
-        fout.open (vm["output"].as<string>().c_str ());
-        if (! fout.is_open ()) {
-            cerr << "Failed to open output file." << endl;
-            return EXIT_FAILURE;
-        }
-
-        os.rdbuf (fout.rdbuf ());
+    ~ScopedAsmLinkingUnits () {
+        SharemindAssemblerLinkingUnits_destroy_with(this,
+            &SharemindAssemblerLinkingUnit_destroy);
     }
+};
 
-    /* Parse the program: */
-    std::auto_ptr<SecreC::TreeNodeModule> parseTree;
+/*
+ * RAII to force C style free. auto_ptr uses delete which is incorrect.
+ */
+class ScopedFree : boost::noncopyable {
+public:
+    explicit ScopedFree (void* p)
+        : m_ptr (p) { }
+    ~ScopedFree () { free (m_ptr); }
+    void* get () const { return m_ptr; }
+private:
+    void* const m_ptr;
+};
+
+
+/*
+ * Collection of program options for a nice overview.
+ */
+struct ProgramOptions {
+    bool                     showHelp;
+    bool                     verbose;
+    bool                     assembleOnly;
+    boost::optional<string>  output; // nothing if cout
+    boost::optional<string>  input; // nothing if cin
+    vector<string>           includes;
+
+    ProgramOptions ()
+        : showHelp (false)
+        , verbose (false)
+        , assembleOnly (false)
+    { }
+};
+
+/*
+ * Parse program options. Returns false on failure.
+ */
+bool readProgramOptions (int argc, char *argv[], ProgramOptions* opts) {
+
+     po::options_description desc ("Available options");
+     desc.add_options ()
+             ("help,h", "Display this help message.")
+             ("verbose,v", "Enable verbose output.")
+             ("include,I", po::value<vector<string > >(), "Directory for module search path.")
+             ("assemble,S", "Output assembly.")
+             ("compile,c", "Compile to bytecode.")
+             ("output,o", po::value<string>(), "Output file.")
+             ("input", po::value<string>(), "Input file.")
+             ;
+     po::positional_options_description p;
+     p.add("input", -1);
+     po::variables_map vm;
+
+     try {
+         po::store(po::command_line_parser (argc, argv).
+                   options (desc).positional (p).run (), vm);
+         po::notify(vm);
+     }
+     catch (const std::exception& e) {
+         cerr << e.what () << endl;
+         cerr << desc << endl;
+         return false;
+     }
+     catch (...) {
+         cerr << desc << endl;
+         return false;
+     }
+
+     if (vm.count ("help")) {
+        opts->showHelp = true;
+        cout << desc << endl;
+     }
+
+     opts->verbose = vm.count ("verbose");
+     opts->assembleOnly = vm.count ("assemble");
+     opts->assembleOnly = !vm.count ("compile");
+
+     if (vm.count ("output")) {
+         opts->output = vm["output"].as<string>();
+     }
+
+     if (vm.count ("input")) {
+         opts->input = vm["input"].as<string>();
+     }
+
+     if (vm.count ("include")) {
+         opts->includes = vm["include"].as<vector<string> >();
+     }
+
+    return true;
+}
+
+/*
+ * Parse a secrec program, returns 0 on failure.
+ */
+auto_ptr<SecreC::TreeNodeModule> parseProgram (const ProgramOptions& opts) {
+    SecreC::TreeNodeModule* ast = 0;
     int parseResult = 0;
-    if (vm.count ("input")) {
-        const std::string fname = vm["input"].as<string>();
+
+    if (opts.input) {
+        const std::string& fname = opts.input.get ();
         FILE* f = fopen(fname.c_str (), "r");
         if (f != NULL) {
-            if (verbose) {
+            if (opts.verbose) {
                 cerr << "Parsing file: \"" << fname << "\"... ";
                 cerr << flush;
             }
 
-            SecreC::TreeNodeModule* ast = 0;
             parseResult = sccparse_file (f, &ast);
-            parseTree.reset (ast);
             fclose (f);
         } else {
-            cerr << "Unable to open file: \"" << fname << "\"" << endl;
-            return EXIT_FAILURE;
+            cerr << "Unable to open file \"" << fname << "\" for parsing." << endl;
         }
     }
     else {
         SecreC::TreeNodeModule* ast = 0;
         parseResult = sccparse (&ast);
-        parseTree.reset (ast);
     }
 
     if (parseResult != 0) {
-        return parseResult;
+        cerr << "Parsing failed! Error code " << parseResult << "." << endl;
+    }
+
+    return auto_ptr<SecreC::TreeNodeModule> (ast);
+}
+
+/*
+ * Compile the actual bytecode executable.
+ */
+bool compileExecutable (ostream& os, const VMLinkingUnit& vmlu) {
+    fs::path p = fs::temp_directory_path () / fs::unique_path ();
+
+    ScopedRemovePath scopedRemove (p);
+
+    {
+        io::stream<io::file_sink > fout (p.string ());
+
+        if (! fout.is_open ()) {
+            cerr << "Failed to open a temporary file \"" << p
+                 << "\" for buffering!" << endl;
+            return false;
+        }
+
+        fout << vmlu << flush;
+
+        if (fout.bad ()) {
+            cerr << "Writing to a temporary file \"" << p << "\" failed!" << std::endl;
+            return false;
+        }
+    }
+
+    ScopedAsmLinkingUnits lus;
+
+    {
+        io::stream<io::mapped_file_source > fin (p.string ());
+        if (! fin.is_open ()) {
+            cerr << "Failed to mmap a temporary file file \"" << p
+                 << "\" for reading!" << endl;
+            return false;
+        }
+
+        size_t sl = 0u;
+        size_t sc = 0u;
+        ScopedAsmTokens ts (sharemind_assembler_tokenize (fin->data (), fin->size (), &sl, &sc));
+        if (ts.get () == 0) {
+            cerr << "ICE: Tokenization failed at (" << sl << "," << sc << ")" << endl;
+            return false;
+        }
+
+        const SharemindAssemblerToken* errorToken = 0;
+        char* errorString = 0;
+        SharemindAssemblerError r = sharemind_assembler_assemble (ts.get (), &lus, &errorToken, &errorString);
+        if (r != SHAREMIND_ASSEMBLE_OK) {
+            const char* smasErrorStr = SharemindAssemblerError_toString (r);
+            assert (smasErrorStr);
+
+            cerr << "Error: ";
+            if (errorToken) {
+                cerr << '(' << errorToken->start_line << ", "
+                            << errorToken->start_column << ", "
+                            << SharemindAssemblerTokenType_toString(errorToken->type) + 11
+                     << ')';
+            }
+
+            cerr << smasErrorStr;
+            if (errorString) cerr <<  ": " << errorString;
+            cerr << endl;
+            free (errorString);
+            return false;
+        }
+    }
+
+    size_t outputLength = 0;
+    ScopedFree ptr (sharemind_assembler_link (0x0, &lus, &outputLength, 0));
+    os.write (static_cast<const char*>(ptr.get ()), outputLength);
+    return true;
+}
+
+} // anonymous namespace
+
+
+int main(int argc, char *argv[]) {
+    ProgramOptions opts;
+
+    if (! readProgramOptions (argc, argv, &opts)) {
+        return EXIT_FAILURE;
+    }
+
+    if (opts.showHelp) {
+        return EXIT_SUCCESS;
+    }
+
+    /* Get output stream: */
+    ostream os (cout.rdbuf ());
+    io::stream_buffer<io::file_sink> fileBuf;
+    if (opts.output) {
+        ios_base::openmode mode;
+        if (! opts.assembleOnly) {
+            mode |= ios_base::binary;
+        }
+
+        fileBuf.open (opts.output.get (), mode);
+        if (! fileBuf.is_open ()) {
+            cerr << "Failed to open output file." << endl;
+            return EXIT_FAILURE;
+        }
+
+        os.rdbuf (&fileBuf);
+    }
+
+    /* Parse the program: */
+    std::auto_ptr<SecreC::TreeNodeModule> parseTree = parseProgram (opts);
+    if (parseTree.get () == 0) {
+        return EXIT_FAILURE;
     }
     
     /* Translate to intermediate code: */
@@ -116,22 +336,31 @@ int main(int argc, char *argv[]) {
     SecreC::ICode icode;
 
     /* Collect possible include files: */
-    if (vm.count ("include")) {
-        BOOST_FOREACH (const string& name, vm["include"].as<vector<string > >()) {
-            icode.modules ().addSearchPath (name);
+    BOOST_FOREACH (const string& name, opts.includes) {
+        if (! icode.modules ().addSearchPath (name) && opts.verbose) {
+            cerr << "Invalid search path \"" << name << "\"." << endl;
         }
     }
 
     if (icode.init (context, parseTree.get ()) != SecreC::ICode::OK) {
-        ::operator << (cerr << "Error generating valid intermediate code." << endl
-                      , icode.compileLog ());
+        cerr << "Error generating valid intermediate code." << endl;
+        cerr << icode.compileLog () << endl;
         return EXIT_FAILURE;
     }
 
-    /* Compiler: */
+    /* Compile: */
     VMLinkingUnit vmlu;
     Compiler compiler (icode);
     compiler.run (vmlu);
-    os << vmlu << endl;
+
+    if (opts.assembleOnly) {
+        os << vmlu << endl;
+    }
+    else {
+        if (! compileExecutable (os, vmlu)) {
+            return EXIT_FAILURE;
+        }
+    }
+
     return EXIT_SUCCESS;
 }
