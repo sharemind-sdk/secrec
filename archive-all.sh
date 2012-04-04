00e85b8ce2354c7a1dbe07c8320db5d17f1ab10f
Jaak Ristioja <jaak.ristioja@cyber.ee>
2012-04-04 13:58:02 +0300

    Added script for archieving the repository and all submodules.

---
+++ archive-all.sh
@@ -0,0 +1,99 @@
+#!/bin/sh
+#
+#  This file is a part of the Sharemind framework.
+#  Copyright (C) Cybernetica AS
+#
+#  All rights are reserved. Reproduction in whole or part is prohibited
+#  without the written consent of the copyright owner. The usage of this
+#  code is subject to the appropriate license agreement.
+#
+
+EXIT_FAILURE=1
+EXIT_SUCCESS=0
+PREFIX=
+
+OLDPATH=`pwd`
+SCRIPT_FULLNAME="$0"
+SCRIPT_PATH="`dirname \"$0\"`"
+
+function usage {
+  SCRIPT_NAME=${SCRIPT_FULLNAME:${#SCRIPT_PATH}}
+  SCRIPT_NAME=${SCRIPT_NAME:1}
+  echo
+  echo 'Usage:'
+  echo
+  echo "    $SCRIPT_NAME --prefix=<prefix>"
+  echo
+  echo 'This script archieves the git repository in the path where it resides,'
+  echo 'including all submodules in ext/* directories. All archieved paths are'
+  echo 'prefixed with "<prefix>/" and output is written to "<prefix>.tar.bz2"'
+  echo 'relative to the current directory.'
+  echo
+}
+
+# Handle command-line arguments:
+shopt -s extglob
+while test $# -gt 0; do
+case $1 in
+
+        --prefix=* )
+            PREFIX="${1:9}"
+            shift
+            ;;
+
+        -h | -? | --help )
+            usage
+            exit $EXIT_SUCCESS
+            ;;
+
+        * )
+            echo "Invalid argument: $1" >&2
+            usage
+            exit $EXIT_FAILURE
+            ;;
+    esac
+done
+
+if [ -z "$PREFIX" ]; then
+  echo 'Error: --prefix not specified!' >&2
+  usage
+  exit $EXIT_FAILURE
+fi
+
+OUTFILE="${PREFIX}.tar.bz2"
+
+if [ -e "$OUTFILE" ]; then
+  echo "Error: The file ${OUTFILE} already exists!" >&2
+  usage
+  exit $EXIT_FAILURE
+fi
+
+cd "$SCRIPT_PATH" # Go to script path
+
+TMPOUTFILE=`mktemp`
+git archive "--prefix=${PREFIX}/" --format=tar HEAD -o "$TMPOUTFILE"
+
+TMPSUBMODULEFILE=`mktemp`
+for SUBMODULEPATH in ext/*; do
+  > "$TMPSUBMODULEFILE"
+  cd "$SUBMODULEPATH" # Go to submodule path
+  git archive --prefix="${PREFIX}/${SUBMODULEPATH}/" --format=tar HEAD -o "$TMPSUBMODULEFILE"
+  cd - 2>&1 > /dev/null # Return from submodule path
+  tar --concatenate "--file=$TMPOUTFILE" "$TMPSUBMODULEFILE"
+done
+rm -f "$TMPSUBMODULEFILE"
+
+cd "$OLDPATH" # Return to caller path
+
+bzip2 --best "$TMPOUTFILE" --stdout > "$OUTFILE"
+rm -f "$TMPOUTFILE"
+
+BYTES=`stat '--format=%s' "$OUTFILE"`
+echo 'Details:'
+echo "  Filename:  ${OUTFILE}"
+echo "  File size: $BYTES bytes`echo $BYTES|awk '{s[2**30]="G";s[2**20]="M";hum[1024]="k";for(x=2**30;x>=1024;x/=1024){if($1>=x){printf " (%.2f %sB)\n",$1/x,s[x];break}}}'`"
+echo "  MD5SUM:    `md5sum \"${OUTFILE}\" |cut -b -32`"
+echo "  SHA1SUM:   `sha1sum \"${OUTFILE}\" |cut -b -40`"
+echo "  SHA256SUM: `sha256sum \"${OUTFILE}\" |cut -b -64`"
+
+exit $EXIT_SUCCESS
