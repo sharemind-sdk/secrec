babc901f49f569beb582cf5048c5394a1105458f
Jaak Ristioja <jaak.ristioja@cyber.ee>
2012-09-07 14:01:22 +0300

    Some YYLLOC_DEFAULT macro safety.

--- include/libscc/parser.h
+++ include/libscc/parser.h
@@ -224,13 +224,13 @@ typedef struct YYLTYPE {
 #define YYLLOC_DEFAULT(Current, Rhs, N) \
     do { \
         if ((N)) { \
-            (Current).first_line   = YYRHSLOC(Rhs, 1).first_line; \
-            (Current).first_column = YYRHSLOC(Rhs, 1).first_column; \
-            (Current).last_line    = YYRHSLOC(Rhs, N).last_line; \
-            (Current).last_column  = YYRHSLOC(Rhs, N).last_column; \
+            (Current).first_line   = YYRHSLOC((Rhs), 1).first_line; \
+            (Current).first_column = YYRHSLOC((Rhs), 1).first_column; \
+            (Current).last_line    = YYRHSLOC((Rhs), (N)).last_line; \
+            (Current).last_column  = YYRHSLOC((Rhs), (N)).last_column; \
         } else { \
-            (Current).first_line = (Current).last_line = YYRHSLOC (Rhs, 0).last_line; \
-            (Current).first_column = (Current).last_column = YYRHSLOC (Rhs, 0).last_column; \
+            (Current).first_line = (Current).last_line = YYRHSLOC((Rhs), 0).last_line; \
+            (Current).first_column = (Current).last_column = YYRHSLOC((Rhs), 0).last_column; \
         } \
         (Current).filename = fileName; \
     } while (0)
