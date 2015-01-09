b12bbb5a8a3f0faab77d1466465e673cbe7c6686
Jaak Ristioja <jaak.ristioja@cyber.ee>
2015-01-09 16:13:10 +0200

    Updated copyright information.

--- src/libscc/analysis/LiveMemory.cpp
+++ src/libscc/analysis/LiveMemory.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 Cybernetica
+ *
+ * Research/Commercial License Usage
+ * Licensees holding a valid Research License or Commercial License
+ * for the Software may use this file according to the written
+ * agreement between you and Cybernetica.
+ *
+ * GNU General Public License Usage
+ * Alternatively, this file may be used under the terms of the GNU
+ * General Public License version 3.0 as published by the Free Software
+ * Foundation and appearing in the file LICENSE.GPL included in the
+ * packaging of this file.  Please review the following information to
+ * ensure the GNU General Public License version 3.0 requirements will be
+ * met: http://www.gnu.org/copyleft/gpl-3.0.html.
+ *
+ * For further information, please contact us at sharemind@cyber.ee.
+ */
+
 #include "analysis/LiveMemory.h"
 
 #include "Symbol.h"
