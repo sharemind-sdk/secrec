6d297e1d0618feff2cf54466a5bee56488efbfa6
Karl Tarbe <karl.tarbe@cyber.ee>
2018-06-20 10:27:02 +0300

    Packaging: Use correct ABI version for libmpfr

--- CMakeIncludes/FindMPFR.cmake
+++ CMakeIncludes/FindMPFR.cmake
@@ -26,6 +26,8 @@
 #  MPFR_FOUND: Boolean that indicates if the package was found
 #  MPFR_INCLUDE_DIRS: Paths to the necessary header files
 #  MPFR_LIBRARIES: Package libraries
+#  MPFR_VERSION: MPFR version
+#  MPFR_ABI_VERSION: MPFR ABI version (either 4 or 6)
 #
 ################################################################################
 
@@ -60,13 +62,43 @@ FIND_LIBRARY(
         lib
 )
 
+IF(MPFR_INCLUDE_DIR)
+    # Query MPFR_VERSION
+    FILE(READ "${MPFR_INCLUDE_DIR}/mpfr.h" _mpfr_header)
+
+    STRING(REGEX MATCH "define[ \t]+MPFR_VERSION_MAJOR[ \t]+([0-9]+)"
+        _mpfr_major_version_match "${_mpfr_header}")
+    SET(MPFR_MAJOR_VERSION "${CMAKE_MATCH_1}")
+    STRING(REGEX MATCH "define[ \t]+MPFR_VERSION_MINOR[ \t]+([0-9]+)"
+        _mpfr_minor_version_match "${_mpfr_header}")
+    SET(MPFR_MINOR_VERSION "${CMAKE_MATCH_1}")
+    STRING(REGEX MATCH "define[ \t]+MPFR_VERSION_PATCHLEVEL[ \t]+([0-9]+)"
+        _mpfr_patchlevel_version_match "${_mpfr_header}")
+    SET(MPFR_PATCHLEVEL_VERSION "${CMAKE_MATCH_1}")
+
+    SET(MPFR_VERSION
+        ${MPFR_MAJOR_VERSION}.${MPFR_MINOR_VERSION}.${MPFR_PATCHLEVEL_VERSION})
+
+    IF(${MPFR_VERSION} VERSION_LESS "3.0.0")
+        SET(MPFR_VERSION_OK FALSE)
+        MESSAGE(STATUS "MPFR version ${MPFR_VERSION} found in ${MPFR_INCLUDES}, "
+            "but at least version 3.0.0 is required")
+    ELSEIF(${MPFR_VERSION} VERSION_LESS "4.0.0")
+        SET(MPFR_VERSION_OK TRUE)
+        SET(MPFR_ABI_VERSION "4")
+    ELSE()
+        SET(MPFR_VERSION_OK TRUE)
+        SET(MPFR_ABI_VERSION "6")
+    ENDIF()
+
+ENDIF()
 
 # Set MPFR_FOUND honoring the QUIET and REQUIRED arguments
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(
     mpfr
     "Could NOT find MPFR"
-    MPFR_LIBRARY MPFR_INCLUDE_DIR)
+    MPFR_LIBRARY MPFR_INCLUDE_DIR MPFR_VERSION_OK)
 
 # Output variables
 IF(MPFR_FOUND)
