sharemind-sdk/secrec

commit bef853d482d2069e24131c9708104ad3f43dfacd

[scc] Compiler: Moved unnecessary API from header to .cpp file

src/scc/Compiler.cpp
@@ -292,11 +292,91 @@ std::string SyscallName::basic (const TypeNonVoid* ty, const char* name,  bool n
     return scname.str ();
 }
 
-} // namespace anonymous
+class Compiler {
 
-/*******************************************************************************
-  Compiler
-*******************************************************************************/
+public: /* Methods: */
+
+    explicit Compiler (bool optimize);
+    Compiler (const Compiler&) = delete;
+    Compiler& operator = (const Compiler&) = delete;
+    ~Compiler ();
+
+    void run (VMLinkingUnit& vmlu, SecreC::ICode& code);
+
+
+private:
+    VMSymbolTable& st () { return m_st; }
+
+    void cgProcedure (const SecreC::Procedure& blocks);
+    void cgBlock (VMFunction& func, const SecreC::Block& block);
+    void cgImop (VMBlock& block, const SecreC::Imop& imop);
+
+    /**
+     * Code generation for various intermediate instructions:
+     */
+    void cgDeclare (VMBlock& block, const SecreC::Imop& imop);
+    void cgJump (VMBlock& block, const SecreC::Imop& imop);
+    void cgAssign (VMBlock& block, const SecreC::Imop& imop);
+    void cgCast (VMBlock& block, const SecreC::Imop& imop);
+    void cgClassify (VMBlock& block, const SecreC::Imop& imop);
+    void cgDeclassify (VMBlock& block, const SecreC::Imop& imop);
+    void cgCopy (VMBlock& block, const SecreC::Imop& imop);
+    void cgCall (VMBlock& block, const SecreC::Imop& imop);
+    void cgGetFpuState (VMBlock& block, const SecreC::Imop& imop);
+    void cgSetFpuState (VMBlock& block, const SecreC::Imop& imop);
+    void cgParam (VMBlock& block, const SecreC::Imop& imop);
+    void cgReturn (VMBlock& block, const SecreC::Imop& imop);
+    void cgArithm (VMBlock& block, const SecreC::Imop& imop);
+    void cgAlloc (VMBlock& block, const SecreC::Imop& imop);
+    void cgRelease (VMBlock& block, const SecreC::Imop& imop);
+    void cgStore (VMBlock& block, const SecreC::Imop& imop);
+    void cgLoad (VMBlock& block, const SecreC::Imop& imop);
+    void cgSyscall (VMBlock& block, const SecreC::Imop& imop);
+    void cgSyscallOperand (VMBlock& block, const SecreC::SyscallOperand& op);
+    void cgDomainID (VMBlock& block, const SecreC::Imop& imop);
+    void cgError (VMBlock& block, const SecreC::Imop& imop);
+    void cgPrint (VMBlock& block, const SecreC::Imop& imop);
+    void cgComment(VMBlock & block, const SecreC::Imop & imop);
+    void cgToString (VMBlock& block, const SecreC::Imop& imop);
+    void cgStrlen (VMBlock& block, const SecreC::Imop& imop);
+
+
+    /**
+     * Operations performed through syscalls:
+     */
+    void cgNewPrivate (VMBlock& block, const SecreC::Symbol* dest, const SecreC::Symbol* size);
+    void cgNewPrivateScalar (VMBlock& block, const SecreC::Symbol* dest);
+    void emitSyscall (VMBlock& block, VMValue* dest, const std::string& name);
+    void emitSyscall (VMBlock& block, const std::string& name);
+    void cgPrivateAssign (VMBlock& block, const SecreC::Imop& imop);
+    void cgPrivateCopy (VMBlock& block, const SecreC::Imop& imop);
+    void cgPrivateAlloc (VMBlock& block, const SecreC::Imop& imop);
+    void cgPrivateRelease (VMBlock& block, const SecreC::Imop& imop);
+    void cgPrivateLoad (VMBlock& block, const SecreC::Imop& imop);
+    void cgPrivateStore (VMBlock& block, const SecreC::Imop& imop);
+
+    /**
+     * Convenience operations:
+     */
+    VMValue* loadToRegister (VMBlock& block, const SecreC::Symbol* symbol);
+    void pushString (VMBlock& block, const SecreC::Symbol* str, bool asNullTerminated = true);
+    void paramString (VMBlock& block, const SecreC::Symbol* str);
+    VMValue* find (const SecreC::Symbol* sym) const;
+    void cgStringAppend (VMBlock& block, const SecreC::Imop& imop);
+    void cgStringCmp (VMBlock& block, const SecreC::Imop& imop);
+
+
+private: /* Fields: */
+
+    VMCodeSection*        m_target;   ///< Target code
+    VMSymbolTable         m_st;       ///< VM symbol table
+    unsigned              m_param;    ///< Current param count
+    BuiltinFunctions*     m_funcs;    ///< Bult-in functions
+    RegisterAllocator*    m_ra;       ///< Register allocator
+    SyscallManager*       m_scm;      ///< The syscall manager
+    StringLiterals*       m_strLit;   ///< String literals
+    bool                  m_optimize; ///< If we need to optimize the code.
+};
 
 Compiler::Compiler (bool optimize)
     : m_target (nullptr)
@@ -309,19 +389,10 @@ Compiler::Compiler (bool optimize)
 { }
 
 Compiler::~Compiler () {
-    cleanup();
-}
-
-void Compiler::cleanup() {
     delete m_funcs;
     delete m_ra;
     delete m_scm;
     delete m_strLit;
-
-    m_funcs = nullptr;
-    m_ra = nullptr;
-    m_scm = nullptr;
-    m_strLit = nullptr;
 }
 
 void Compiler::run (VMLinkingUnit& vmlu, SecreC::ICode& code) {
@@ -380,8 +451,6 @@ void Compiler::run (VMLinkingUnit& vmlu, SecreC::ICode& code) {
 
     m_funcs->generateAll (*m_target, m_st);
     m_target->setNumGlobals (m_ra->globalCount ());
-
-    cleanup();
 }
 
 VMValue* Compiler::find (const SecreC::Symbol* sym) const {
@@ -1276,5 +1345,10 @@ void Compiler::cgPrivateStore (VMBlock& block, const Imop& imop) {
     block.push_new () << "push" << find (imop.dest ());
     emitSyscall (block, SyscallName::basic (ty, "store"));
 }
+
+} // anonymous namespace
+
+void compile(VMLinkingUnit & vmlu, SecreC::ICode & code, bool optimize)
+{ Compiler(optimize).run(vmlu, code); }
 
 } // namespace SecreCC

src/scc/Compiler.h
@@ -20,118 +20,12 @@
 #ifndef CODEGEN_H
 #define CODEGEN_H
 
-#include "VMCode.h"
-#include "VMSymbolTable.h"
-
-#include <set>
-#include <map>
-
-namespace SecreC {
-class Procedure;
-class Block;
-class Imop;
-class ICode;
-class SyscallOperand;
-} /* namespace SecreC { */
-
-
+namespace SecreC { class ICode; }
 namespace SecreCC {
 
-class BuiltinFunctions;
-class RegisterAllocator;
-class SyscallManager;
-class StringLiterals;
+class VMLinkingUnit;
 
-/*******************************************************************************
-  Compiler
-*******************************************************************************/
-
-class __attribute__ ((visibility("internal"))) Compiler {
-public: /* Methods: */
-
-    explicit Compiler (bool optimize);
-    Compiler (const Compiler&) = delete;
-    Compiler& operator = (const Compiler&) = delete;
-    ~Compiler ();
-
-    void run (VMLinkingUnit& vmlu, SecreC::ICode& code);
-
-    VMSymbolTable& st () { return m_st; }
-
-private:
-
-    void cleanup();
-
-    void cgProcedure (const SecreC::Procedure& blocks);
-    void cgBlock (VMFunction& func, const SecreC::Block& block);
-    void cgImop (VMBlock& block, const SecreC::Imop& imop);
-
-    /**
-     * Code generation for various intermediate instructions:
-     */
-    void cgDeclare (VMBlock& block, const SecreC::Imop& imop);
-    void cgJump (VMBlock& block, const SecreC::Imop& imop);
-    void cgAssign (VMBlock& block, const SecreC::Imop& imop);
-    void cgCast (VMBlock& block, const SecreC::Imop& imop);
-    void cgClassify (VMBlock& block, const SecreC::Imop& imop);
-    void cgDeclassify (VMBlock& block, const SecreC::Imop& imop);
-    void cgCopy (VMBlock& block, const SecreC::Imop& imop);
-    void cgCall (VMBlock& block, const SecreC::Imop& imop);
-    void cgGetFpuState (VMBlock& block, const SecreC::Imop& imop);
-    void cgSetFpuState (VMBlock& block, const SecreC::Imop& imop);
-    void cgParam (VMBlock& block, const SecreC::Imop& imop);
-    void cgReturn (VMBlock& block, const SecreC::Imop& imop);
-    void cgArithm (VMBlock& block, const SecreC::Imop& imop);
-    void cgAlloc (VMBlock& block, const SecreC::Imop& imop);
-    void cgRelease (VMBlock& block, const SecreC::Imop& imop);
-    void cgStore (VMBlock& block, const SecreC::Imop& imop);
-    void cgLoad (VMBlock& block, const SecreC::Imop& imop);
-    void cgSyscall (VMBlock& block, const SecreC::Imop& imop);
-    void cgSyscallOperand (VMBlock& block, const SecreC::SyscallOperand& op);
-    void cgDomainID (VMBlock& block, const SecreC::Imop& imop);
-    void cgError (VMBlock& block, const SecreC::Imop& imop);
-    void cgPrint (VMBlock& block, const SecreC::Imop& imop);
-    void cgComment(VMBlock & block, const SecreC::Imop & imop);
-    void cgToString (VMBlock& block, const SecreC::Imop& imop);
-    void cgStrlen (VMBlock& block, const SecreC::Imop& imop);
-
-
-    /**
-     * Operations performed through syscalls:
-     */
-    void cgNewPrivate (VMBlock& block, const SecreC::Symbol* dest, const SecreC::Symbol* size);
-    void cgNewPrivateScalar (VMBlock& block, const SecreC::Symbol* dest);
-    void emitSyscall (VMBlock& block, VMValue* dest, const std::string& name);
-    void emitSyscall (VMBlock& block, const std::string& name);
-    void cgPrivateAssign (VMBlock& block, const SecreC::Imop& imop);
-    void cgPrivateCopy (VMBlock& block, const SecreC::Imop& imop);
-    void cgPrivateAlloc (VMBlock& block, const SecreC::Imop& imop);
-    void cgPrivateRelease (VMBlock& block, const SecreC::Imop& imop);
-    void cgPrivateLoad (VMBlock& block, const SecreC::Imop& imop);
-    void cgPrivateStore (VMBlock& block, const SecreC::Imop& imop);
-
-    /**
-     * Convenience operations:
-     */
-    VMValue* loadToRegister (VMBlock& block, const SecreC::Symbol* symbol);
-    void pushString (VMBlock& block, const SecreC::Symbol* str, bool asNullTerminated = true);
-    void paramString (VMBlock& block, const SecreC::Symbol* str);
-    VMValue* find (const SecreC::Symbol* sym) const;
-    void cgStringAppend (VMBlock& block, const SecreC::Imop& imop);
-    void cgStringCmp (VMBlock& block, const SecreC::Imop& imop);
-
-
-private: /* Fields: */
-
-    VMCodeSection*        m_target;   ///< Target code
-    VMSymbolTable         m_st;       ///< VM symbol table
-    unsigned              m_param;    ///< Current param count
-    BuiltinFunctions*     m_funcs;    ///< Bult-in functions
-    RegisterAllocator*    m_ra;       ///< Register allocator
-    SyscallManager*       m_scm;      ///< The syscall manager
-    StringLiterals*       m_strLit;   ///< String literals
-    bool                  m_optimize; ///< If we need to optimize the code.
-};
+void compile(VMLinkingUnit & vmlu, SecreC::ICode & code, bool optimize);
 
 } // namespace SecreCC
 

src/scc/main.cpp
@@ -47,6 +47,7 @@
 #include <sharemind/PotentiallyVoidTypeInfo.h>
 
 #include "Compiler.h"
+#include "VMCode.h"
 
 using namespace std;
 using namespace SecreCC;
@@ -292,7 +293,6 @@ int main (int argc, char *argv[]) {
         if (opts.showHelp)
             return EXIT_SUCCESS;
 
-        Compiler compiler { opts.optimize };
         VMLinkingUnit vmlu;
 
         {
@@ -328,7 +328,7 @@ int main (int argc, char *argv[]) {
                 return EXIT_SUCCESS;
 
             /* Compile: */
-            compiler.run (vmlu, icode);
+            compile(vmlu, icode, opts.optimize);
         }
 
         /* Output: */

commit c79abe42290515c077cf469458a48c008b6ec926

[scc] VMCode: Store shared pointers to OStreamables instead of pointers to VMLabel
Commit bef853d482d2069e24131c9708104ad3f43dfacd inadvertently caused some
use-after-free due to Compiler being out-of-scope when outputting the results
in main.cpp. One of the issues causing that were these VMLabel pointers which
ended up dangling.

src/scc/Builtin.cpp
@@ -57,7 +57,7 @@ void BuiltinFunctions::generateAll (VMCodeSection& code, VMSymbolTable& st) {
     for (auto & f : m_functions)
         vs.emplace(ValueType{f.first, *f.second});
     for (auto const & v : vs) {
-        VMFunction function(v.label);
+        VMFunction function(v.label->nameStreamable());
         v.function.get().generate(function, st);
         code.push_back(function);
     }
@@ -79,7 +79,7 @@ void BuiltinAlloc::generate (VMFunction& function, VMSymbolTable& st) {
     auto const lBack = st.getUniqLabel(":back_");
     auto const lOut = st.getUniqLabel(":out_");
 
-    VMBlock entryB(nullptr, nullptr);
+    VMBlock entryB;
     entryB.push_new () << "resizestack" << 4;
     entryB.push_new () << "mov imm 0x0" << rOffset;
     entryB.push_new () << "bmul uint64" << rSize << iSize;
@@ -89,14 +89,14 @@ void BuiltinAlloc::generate (VMFunction& function, VMSymbolTable& st) {
     entryB.push_new () << "udec uint64" << rSize;
     function.push_back (entryB);
 
-    VMBlock middleB(lBack, nullptr);
+    VMBlock middleB(lBack->nameStreamable());
     middleB.push_new () << "jge" << lOut << "uint64" << rOffset << rSize;
     middleB.push_new () << "mov" << rDefault << "mem" << rOut << rOffset << iSize;
     middleB.push_new () << "badd uint64" << rOffset << iSize;
     middleB.push_new () << "jmp" << lBack;
     function.push_back (middleB);
 
-    VMBlock exitB(lOut, nullptr);
+    VMBlock exitB(lOut->nameStreamable());
     exitB.push_new () << "return" << rOut;
     function.push_back (exitB);
 }
@@ -118,7 +118,7 @@ void BuiltinVArith::generate (VMFunction& function, VMSymbolTable& st) {
     VMImm* const argSize = st.getImm (sizeInBytes (argTy));
     VMImm* const destSize = st.getImm (sizeInBytes (destTy));
 
-    VMBlock entryB(nullptr, nullptr);
+    VMBlock entryB;
 
     entryB.push_new () << "resizestack" << (2*n + 3);
 
@@ -139,7 +139,7 @@ void BuiltinVArith::generate (VMFunction& function, VMSymbolTable& st) {
     auto const lOut = st.getUniqLabel(":out_");
 
     // jump out if needed
-    VMBlock middleB(lBack, nullptr);
+    VMBlock middleB(lBack->nameStreamable());
     middleB.push_new () << "jge" << lOut << "uint64" << rCount << rSize;
 
     // move arguments to temporaries
@@ -205,7 +205,7 @@ void BuiltinVArith::generate (VMFunction& function, VMSymbolTable& st) {
     // jump back to conditional
     middleB.push_new () << "jmp" << lBack;
 
-    VMBlock returnB(lOut, nullptr);
+    VMBlock returnB(lOut->nameStreamable());
     returnB.push_new () << "return imm 0x0";
 
     function.push_back (entryB);
@@ -226,7 +226,7 @@ void BuiltinFloatToInt::generate (VMFunction& function, VMSymbolTable& st) {
     VMStack* prevFpuState = st.getStack(2);
     VMStack* fpuState = st.getStack(3);
 
-    VMBlock block(nullptr, nullptr);
+    VMBlock block;
     block.push_new () << "resizestack" << 4;
 
     // Set proper FPU state:
@@ -281,7 +281,7 @@ void BuiltinVCast::generate (VMFunction& function, VMSymbolTable& st) {
 
     ///////////////
     // Entry block:
-    VMBlock entryB(nullptr, nullptr);
+    VMBlock entryB;
 
     entryB.push_new () << "resizestack" << stackSize;
 
@@ -298,7 +298,7 @@ void BuiltinVCast::generate (VMFunction& function, VMSymbolTable& st) {
     entryB.push_new () << "jge" << exitL << VM_UINT64 << srcOff << size;
 
 
-    VMBlock middleB(middleL, nullptr);
+    VMBlock middleB(middleL->nameStreamable());
     middleB.push_new () << "mov" << "mem" << src << srcOff << temp << srcSize;
     middleB.push_new () << "convert" << m_src << temp << m_dest << temp;
     middleB.push_new () << "mov" << temp << "mem" << dest << destOff << destSize;
@@ -306,7 +306,7 @@ void BuiltinVCast::generate (VMFunction& function, VMSymbolTable& st) {
     middleB.push_new () << "badd" << VM_UINT64 << destOff << destSize;
     middleB.push_new () << "jlt" << middleL << VM_UINT64 << srcOff << size;
 
-    VMBlock exitB(exitL, nullptr);
+    VMBlock exitB(exitL->nameStreamable());
 
     if (needToRoundToZero) {
         fpuSetState(exitB, prevFpuState);
@@ -342,14 +342,14 @@ void BuiltinVBoolCast::generate (VMFunction& function, VMSymbolTable& st) {
 
     ///////////////
     // Entry block:
-    VMBlock entryB(nullptr, nullptr);
+    VMBlock entryB;
     entryB.push_new () << "resizestack" << 7;
     entryB.push_new () << "mov" << st.getImm (0) << srcOff;
     entryB.push_new () << "mov" << st.getImm (0) << destOff;
     entryB.push_new () << "bmul uint64" << size << srcSize;
     entryB.push_new () << "jge" << exitL << "uint64" << srcOff << size;
 
-    VMBlock middleB(middleL, nullptr);
+    VMBlock middleB(middleL->nameStreamable());
     middleB.push_new () << "mov" << "mem" << src << srcOff << temp << srcSize;
     middleB.push_new () << "tne" << m_src << boolTemp << temp << st.getImm (0);
     middleB.push_new () << "mov" << boolTemp << "mem" << dest << destOff << destElemSize;
@@ -357,7 +357,7 @@ void BuiltinVBoolCast::generate (VMFunction& function, VMSymbolTable& st) {
     middleB.push_new () << "badd uint64" << destOff << destElemSize;
     middleB.push_new () << "jlt" << middleL << "uint64" << srcOff << size;
 
-    VMBlock exitB(exitL, nullptr);
+    VMBlock exitB(exitL->nameStreamable());
     exitB.push_new () << "return imm 0x0";
 
     function.push_back (entryB)
@@ -378,7 +378,7 @@ void BuiltinStrAppend::generate (VMFunction& function, VMSymbolTable& st) {
     VMStack* totalSize = st.getStack (2);
     VMStack* dest = st.getStack (3);
 
-    VMBlock block(nullptr, nullptr);
+    VMBlock block;
     block.push_new () << "resizestack 0x4";
     block.push_new () << "getcrefsize" << lhs << lhsSize;
     block.push_new () << "getcrefsize" << rhs << rhsSize;
@@ -401,7 +401,7 @@ void BuiltinStrDup::generate (VMFunction& function, VMSymbolTable& st) {
     VMStack* size = st.getStack (0);
     VMStack* dest = st.getStack (1);
 
-    VMBlock block(nullptr, nullptr);
+    VMBlock block;
     block.push_new () << "resizestack 0x2";
     block.push_new () << "getcrefsize" << src << size;
     block.push_new () << "alloc" << dest << size;
@@ -425,7 +425,7 @@ void BuiltinBoolToString::generate (VMFunction& function, VMSymbolTable& st) {
     VMStack* dest = st.getStack (4);
     VMLabel* trueL = st.getUniqLabel ();
 
-    VMBlock entryB(nullptr, nullptr);
+    VMBlock entryB;
     entryB.push_new () << "resizestack" << 6;
     entryB.push_new () << "mov" << trueLit << label;
     entryB.push_new () << "mov" << st.getImm (5) << size;
@@ -435,7 +435,7 @@ void BuiltinBoolToString::generate (VMFunction& function, VMSymbolTable& st) {
     entryB.push_new () << "mov" << falseLit << label;
     entryB.push_new () << "mov" << st.getImm (6) << size;
 
-    VMBlock exitB(trueL, nullptr);
+    VMBlock exitB(trueL->nameStreamable());
     exitB.push_new () << "alloc" << dest << size;
     exitB.push_new () << "mov" << "mem" << rodata << label << "mem" << dest << st.getImm (0) << size;
     exitB.push_new () << "return" << dest;
@@ -458,7 +458,7 @@ void BuiltinStringCmp::generate (VMFunction& function, VMSymbolTable& st) {
     VMStack* sizeR = st.getStack (5);
     VMStack* temp = st.getStack (6);
 
-    VMBlock entryB(nullptr, nullptr);
+    VMBlock entryB;
     entryB.push_new () << "resizestack" << 7;
     entryB.push_new () << "mov" << st.getImm (0) << idx;
     // zero the bytes to use wider subtraction
@@ -471,7 +471,7 @@ void BuiltinStringCmp::generate (VMFunction& function, VMSymbolTable& st) {
     VMLabel* falseL = st.getUniqLabel ();
     VMLabel* trueL = st.getUniqLabel ();
 
-    VMBlock loopB(loopL, nullptr);
+    VMBlock loopB(loopL->nameStreamable());
     loopB.push_new () << "mov cref 0x0" << idx << chrL << st.getImm (1);
     loopB.push_new () << "mov cref 0x1" << idx << chrR << st.getImm (1);
     loopB.push_new () << "tsub" << VM_INT64 << temp << chrL << chrR;
@@ -481,10 +481,10 @@ void BuiltinStringCmp::generate (VMFunction& function, VMSymbolTable& st) {
     loopB.push_new () << "jge" << trueL << VM_UINT64 << idx << sizeR;
     loopB.push_new () << "jmp" << loopL;
 
-    VMBlock trueB(trueL, nullptr);
+    VMBlock trueB(trueL->nameStreamable());
     loopB.push_new () << "tsub" << VM_INT64 << temp << sizeL << sizeR;
 
-    VMBlock falseB(falseL, nullptr);
+    VMBlock falseB(falseL->nameStreamable());
     falseB.push_new () << "return" << temp;
 
     function.push_back (entryB)

src/scc/Compiler.cpp
@@ -30,12 +30,13 @@
 #include <libscc/TreeNode.h>
 #include <libscc/Types.h>
 #include <libscc/analysis/LiveVariables.h>
-
 #include "Builtin.h"
 #include "RegisterAllocator.h"
 #include "StringLiterals.h"
 #include "SyscallManager.h"
 #include "VMDataType.h"
+#include "VMValue.h"
+
 
 namespace SecreCC {
 
@@ -460,13 +461,13 @@ void Compiler::paramString (VMBlock& block, const Symbol* dest) {
 }
 
 void Compiler::cgProcedure (const Procedure& blocks) {
-    VMLabel * name = nullptr;
+    VMLabel * label = nullptr;
     if (!blocks.name())
-        name = m_st.getLabel (":start"); // nullptr instead?
+        label = m_st.getLabel (":start"); // nullptr instead?
     else
-        name = getProc (m_st, blocks.name ());
+        label = getProc (m_st, blocks.name ());
     m_param = 0;
-    VMFunction function (name);
+    VMFunction function(label->nameStreamable());
     if (!blocks.name())
         function.setIsStart ();
 
@@ -482,12 +483,11 @@ void Compiler::cgProcedure (const Procedure& blocks) {
 }
 
 void Compiler::cgBlock (VMFunction& function, const Block& block) {
-    VMLabel * name = nullptr;
-    if (block.hasIncomingJumps ()) {
-        name = getLabel (m_st, block);
-    }
+    std::shared_ptr<OStreamable> nameStreamable;
+    if (block.hasIncomingJumps())
+        nameStreamable = getLabel(m_st, block)->nameStreamable();
 
-    VMBlock vmBlock (name, &block);
+    VMBlock vmBlock(nameStreamable, &block);
     m_ra.enterBlock (vmBlock);
     for (const Imop& imop : block) {
         cgImop (vmBlock, imop);

src/scc/StringLiterals.cpp
@@ -23,6 +23,8 @@
 
 #include "VMSymbolTable.h"
 #include "VMCode.h"
+#include "VMValue.h"
+
 
 namespace {
 
@@ -84,7 +86,7 @@ StringLiterals::LiteralInfo StringLiterals::insert (const std::string& str, bool
         os << ":STR_" << m_uniq ++;
         VMLabel* label = m_st.getLabel (os.str ());
         const std::string& s = escape (str, asNullTerminated);
-        m_dataSection->addStringRecord(label, s);
+        m_dataSection->addStringRecord(label->nameStreamable(), s);
         auto size = str.size();
         if (asNullTerminated)
             ++size;

src/scc/SyscallManager.cpp
@@ -20,9 +20,10 @@
 #include "SyscallManager.h"
 
 #include <libscc/SecurityType.h>
-
 #include "VMSymbolTable.h"
 #include "VMCode.h"
+#include "VMValue.h"
+
 
 using namespace SecreC;
 
@@ -49,7 +50,8 @@ void SyscallManager::addPd(const SecreC::SymbolDomain * sym) {
         if (i == m_pds.end ()) {
             auto const label = m_st.getUniqLabel(":PD_");
             m_pds.insert (i, std::make_pair (privSecTy, label));
-            m_pdSection->addBinding (label, privSecTy->name ().str());
+            m_pdSection->addBinding(label->nameStreamable(),
+                                    privSecTy->name().str());
         }
     }
 }
@@ -73,7 +75,7 @@ VMLabel* SyscallManager::getSyscallBinding (const std::string& name) {
     if (i == m_syscalls.end ()) {
         auto const label = m_st.getUniqLabel(":SC_");
         i = m_syscalls.insert (i, std::make_pair (name, label));
-        m_scSection->addBinding (label, name);
+        m_scSection->addBinding(label->nameStreamable(), name);
     }
 
     return i->second;

src/scc/VMCode.cpp
@@ -24,7 +24,6 @@
 #include <ostream>
 #include <iterator>
 
-#include "VMValue.h"
 
 namespace SecreCC {
 
@@ -34,7 +33,7 @@ namespace SecreCC {
 
 std::ostream& operator << (std::ostream& os, const VMBlock& block) {
     if (block.m_name)
-        os << block.m_name->name () << '\n';
+        block.m_name->streamTo(os) << '\n';
 
     std::copy (block.begin (), block.end (),
                std::ostream_iterator<VMInstruction>(os, "\n"));
@@ -46,8 +45,7 @@ std::ostream& operator << (std::ostream& os, const VMBlock& block) {
 *******************************************************************************/
 
 std::ostream& operator << (std::ostream& os, const VMFunction& function) {
-    assert(function.m_name);
-    os << function.m_name->name () << '\n';
+    function.m_name->streamTo(os) << '\n';
     if (function.numLocals () != 0) {
         assert (! function.isStart ()
                 && "Must not have local registers in global scope");
@@ -65,8 +63,8 @@ std::ostream& operator << (std::ostream& os, const VMFunction& function) {
 *******************************************************************************/
 
 std::ostream& operator << (std::ostream& os, const VMBinding& binding) {
-    os << binding.m_label->name () << " .bind \"" << binding.m_name << "\"";
-    return  os;
+    return binding.m_label->streamTo(os) << " .bind \"" << binding.m_name
+                                         << "\"";
 }
 
 /*******************************************************************************
@@ -93,7 +91,7 @@ std::ostream& VMBindingSection::printBodyV (std::ostream& os) const {
 
 std::ostream & VMDataSection::StringRecord::print(std::ostream & os) const {
     if (m_label)
-        os << m_label->name () << ' ';
+        m_label->streamTo(os) << ' ';
     return os << ".data string " << m_value;
 }
 

src/scc/VMCode.h
@@ -25,16 +25,17 @@
 #include <list>
 #include <memory>
 #include <ostream>
-
+#include <string>
+#include <utility>
+#include "OStreamable.h"
 #include "VMDataType.h"
+
 
 namespace SecreC {
     class Block;
 } // namespace SecreC
 
 namespace SecreCC {
-
-class VMLabel;
 
 /*******************************************************************************
   VMBlock
@@ -52,10 +53,15 @@ public: /* Types: */
 
 public: /* Methods: */
 
-    VMBlock (const VMLabel* name, const SecreC::Block* block)
-        : m_name (name)
+    VMBlock(std::shared_ptr<OStreamable> name,
+            SecreC::Block const * block = nullptr) noexcept
+        : m_name(std::move(name))
         , m_secrecBlock (block)
-    { }
+    {}
+
+    VMBlock(SecreC::Block const * block = nullptr) noexcept
+        : m_secrecBlock(block)
+    {}
 
     const SecreC::Block* secrecBlock () const { return m_secrecBlock; }
 
@@ -76,7 +82,7 @@ public: /* Methods: */
 
 private: /* Fields: */
 
-    const VMLabel*        const  m_name;
+    std::shared_ptr<OStreamable> const m_name;
     const SecreC::Block*  const  m_secrecBlock;
     InstList                     m_instructions;
 };
@@ -97,11 +103,11 @@ public: /* Types: */
 
 public: /* Methods: */
 
-    explicit VMFunction (const VMLabel* name)
-        : m_name (name)
+    explicit VMFunction(std::shared_ptr<OStreamable> name)
+        : m_name(std::move(name))
         , m_isStart (false)
         , m_numLocals (0)
-    { }
+    {}
 
     unsigned numLocals () const { return m_numLocals; }
     void setNumLocals (unsigned n) { m_numLocals = n; }
@@ -122,7 +128,7 @@ public: /* Methods: */
 private: /* Fields: */
 
    BlockList       m_blocks;  ///< VM blocks that define this function.
-   const VMLabel*  m_name; ///< Name of the function.
+   std::shared_ptr<OStreamable> const m_name; ///< Name of the function.
    bool            m_isStart; ///< Special function to mark the start of the byte code.
    unsigned        m_numLocals; ///< Number of local registers, either "reg" or "stack".
 };
@@ -135,16 +141,16 @@ private: /* Fields: */
 class __attribute__ ((visibility("internal"))) VMBinding {
 public: /* Methods: */
 
-    VMBinding (VMLabel* label, std::string name)
-        : m_label (label)
-        , m_name (name)
+    VMBinding(std::shared_ptr<OStreamable> label, std::string name)
+        : m_label(std::move(label))
+        , m_name(std::move(name))
     { }
 
     friend std::ostream& operator << (std::ostream& os, const VMBinding& binding);
 
 private: /* Fields: */
 
-    VMLabel*    const m_label;
+    std::shared_ptr<OStreamable> const m_label;
     std::string const m_name;
 };
 
@@ -189,9 +195,8 @@ public: /* Methods: */
         : VMSection (name)
     { }
 
-    void addBinding (VMLabel* label, const std::string& name) {
-        m_bindings.push_back (VMBinding (label, name));
-    }
+    void addBinding(std::shared_ptr<OStreamable> label, std::string name)
+    { m_bindings.emplace_back(std::move(label), std::move(name)); }
 
     iterator begin () { return m_bindings.begin (); }
     iterator end () { return m_bindings.end (); }
@@ -225,16 +230,15 @@ public: /* Types: */
     };
 
     struct StringRecord {
-    private:
+    public: /* Methods: */
         StringRecord & operator=(StringRecord const &) = delete;
-    public:
-        StringRecord(VMLabel * label, std::string const & value)
-            : m_label (label)
-            , m_value (value)
-        { }
+        StringRecord(std::shared_ptr<OStreamable> label, std::string value)
+            : m_label(std::move(label))
+            , m_value(std::move(value))
+        {}
 
-        VMLabel*      const  m_label;
-        std::string   const  m_value;
+        std::shared_ptr<OStreamable> const m_label;
+        std::string const m_value;
 
         std::ostream & print(std::ostream & os) const;
     };
@@ -249,8 +253,8 @@ public: /* Methods: */
         : VMSection (type == RODATA ? "RODATA" : "DATA")
     { }
 
-    void addStringRecord(VMLabel * l, std::string const & v)
-    { m_records.push_back(StringRecord(l, v)); }
+    void addStringRecord(std::shared_ptr<OStreamable> label, std::string value)
+    { m_records.emplace_back(std::move(label), std::move(value)); }
 
 protected:
 